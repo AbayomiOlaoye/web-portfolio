54869c71bd2605ed19e40b63469159f424764eca
Olaoye Abayomi Oluwatoyin <mindprepopps@gmail.com>
2023-06-22 02:10:02 +0100

    Fix navigation issues

--- components/home/about/About.tsx
+++ components/home/about/About.tsx
@@ -11,39 +11,39 @@ export const About = () => {
       <SectionHeader title="About" dir="l" />
       <div className={styles.about}>
         <div>
-          <Reveal>
+          {/* <Reveal> */}
             <p className={`${styles.aboutText} ${styles.highlightFirstLetter}`}>
               Hi, I&apos;m Oluwatoyin Olaoye, a skilled software developer specializing in Ruby, JavaScript, React,
               and Ruby on Rails. With 2 years worth of experience in 1 year,I excel in building robust web applications.
              </p>
-          </Reveal>
-          <Reveal>
+          {/* </Reveal> */}
+          {/* <Reveal> */}
             <p className={styles.aboutText}>
               I thrive in collaborative environments, utilizing strong communication skills to deliver exceptional results.
               Proficient in backend development with Ruby, I also create captivating user interfaces using JavaScript and React.
               I am well-versed in the entire development lifecycle, from conceptualization to deployment.
             </p>
-          </Reveal>
-          <Reveal>
+          {/* </Reveal>
+          <Reveal> */}
             <p className={styles.aboutText}>
               I prioritize clean and maintainable code, following best practices and test-driven development. I am
               adaptable and embrace agile methodologies to meet changing requirements and deliver high-quality software.
             </p>
-          </Reveal>
-          <Reveal>
+          {/* </Reveal>
+          <Reveal> */}
             <p className={styles.aboutText}>
               When I&apos;m not coding, I&apos;m picking up concepts about design and Data Structures, or you could check for me at The Emirate Stadium. 
               I am probably enjoying Football in England. 🤓
             </p>
-          </Reveal>
-          <Reveal>
+          {/* </Reveal>
+          <Reveal> */}
             <p className={styles.aboutText}>
               I&apos;m always looking for new challenges and opportunities to merge my passion for learning new things and my love for solving problems. 
-              If you&apos;re interested in working together or have any question, please don&apos;t hesitate to get in touch!
+              If you&apos;re interested in working together or have any questions, please don&apos;t hesitate to get in touch!
               🔗
             </p>
-          </Reveal>
-          <Reveal>
+          {/* </Reveal>
+          <Reveal> */}
             <div className={styles.links}>
               <div className={styles.linksText}>
                 <span>We can connect via </span>
@@ -51,7 +51,7 @@ export const About = () => {
               </div>
               <MyLinks />
             </div>
-          </Reveal>
+          {/* </Reveal> */}
         </div>
         <Stats />
       </div>
--- components/home/hero/Hero.tsx
+++ components/home/hero/Hero.tsx
@@ -11,32 +11,32 @@ export const Hero = () => {
     <section className={`section-wrapper ${styles.hero}`}>
       <div className={styles.heroGrid}>
         <div className={styles.copyWrapper}>
-          <Reveal>
+          {/* <Reveal> */}
             <h1 className={styles.title}>
               Hi, I&apos;m Oluwatoyin<span>.</span>
             </h1>
-          </Reveal>
-          <Reveal>
+          {/* </Reveal>
+          <Reveal> */}
             <h2 className={styles.subTitle}>
               I&apos;m a <span>Full-Stack Developer</span>
             </h2>
-          </Reveal>
-          <Reveal>
+          {/* </Reveal>
+          <Reveal> */}
             <p className={styles.aboutCopy}>
               I&apos;ve spent this past <span>1</span> year coding for over <span>6000+</span> hours, building, and shipping over
-              <span>30</span> projects with Vanilla JavaScript, React, Ruby, Ruby on Rails as group projects, for
+              <span> 30</span> projects with Vanilla JavaScript, React, Ruby, Ruby on Rails as group projects, for
               some pretty cool companies, and individuals to match up to <span>3</span> years industry experience. 
               I also create interesting self projects in my spare time, and I am always open to opportunities.
               Let&apos;s connect!
             </p>
-          </Reveal>
-          <Reveal>
+          {/* </Reveal>
+          <Reveal> */}
             <StandardButton
               onClick={() => document.getElementById("contact")?.scrollIntoView()}
             >
               Contact me
             </StandardButton>
-          </Reveal>
+          {/* </Reveal> */}
         </div>
         <motion.div
           initial={{ y: -30, opacity: 0 }}
@@ -52,7 +52,7 @@ export const Hero = () => {
           />
         </motion.div>
       </div>
-      <DotGrid />
+      {/* <DotGrid /> */}
     </section>
   );
 };
